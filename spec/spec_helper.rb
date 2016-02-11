d35db3d7d804bc450a8cf6991a1757a37a1c00ef
fukuiretu <fukuiretu@gmail.com>
2016-02-11 14:20:54 +0900

    Modify spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,6 @@
+Bundler.require
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start

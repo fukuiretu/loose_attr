5186b12c687a03979e081a8e2c745cd75f1ceec1
fukuiretu <fukuiretu@gmail.com>
2016-02-11 14:38:10 +0900

    Modify spec

--- Gemfile
+++ Gemfile
@@ -3,4 +3,4 @@ source 'https://rubygems.org'
 # Specify your gem's dependencies in loose_attr.gemspec
 gemspec
 
-gem 'codeclimate-test-reporter', group: :test, require: nil
+gem 'codeclimate-test-reporter', '0.4.5', group: :test, require: nil
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,10 +3,4 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 require "codeclimate-test-reporter"
-require 'simplecov'
-
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[SimpleCov::Formatter::HTMLFormatter,CodeClimate::TestReporter::Formatter]
-
-dir = File.join("..", "coverage")
-SimpleCov.coverage_dir(dir)
-SimpleCov.start CodeClimate::TestReporter.configuration.profile
+CodeClimate::TestReporter.start

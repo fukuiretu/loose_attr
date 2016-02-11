9722fde41f8b7775b650a0958b7fea0c207d7ca2
fukuiretu <fukuiretu@gmail.com>
2016-02-11 14:19:06 +0900

    Modify spec helperq

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,4 @@
-Bundler.require
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
-
-RSpec.configure do
-end
